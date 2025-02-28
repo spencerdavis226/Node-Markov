5221b5382f28278de9f96381f7478668acc41fe9
Spencer Davis <spencerdavis226@gmail.com>
2025-02-28 14:30:34 -0500

    Completed Node Markov assignment: implemented MarkovMachine and makeText.js

--- makeText.js
+++ makeText.js
@@ -1,2 +1,52 @@
-/** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const axios = require('axios');
+const { MarkovMachine } = require('./markov');
 
+// Whenever something goes wrong (like failing to read a file or URL), call this helper function
+function errorAndExit(message) {
+  console.error(message);
+  process.exit(1);
+}
+
+// Function to read from a file
+function makeTextFromFile(filePath) {
+  try {
+    return fs.readFileSync(filePath, 'utf8');
+  } catch (err) {
+    errorAndExit(`Error reading file: ${err}`);
+  }
+}
+
+// Function to fetch text from a URL
+async function makeTextFromURL(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (err) {
+    errorAndExit(`Error fetching URL ${err}`);
+  }
+}
+
+async function main() {
+  const args = process.argv.slice(2); // Arguments start at index 2, since 0: 'node' and 1: path to script (eg makeText.js)
+  if (args.length < 2) {
+    errorAndExit(
+      'Usage: node makeText.js file <filename> OR node makeText.js url <url>'
+    );
+  }
+  const [type, pathOrUrl] = args; // type = argv[2] (argument 3), pathOrUrl = argv[3] (argument 4)
+  let text = '';
+
+  if (type === 'file') {
+    text = makeTextFromFile(pathOrUrl);
+  } else if (type === 'url') {
+    text = await makeTextFromURL(pathOrUrl);
+  } else {
+    errorAndExit("First argument must be 'file' or 'url'");
+  }
+
+  const mm = new MarkovMachine(text);
+  console.log(mm.makeText());
+}
+
+main();
